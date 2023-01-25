9ab2b4705474459200ea1c88b5deacf49eda8220
-Brownlolaoo <-aramyeedey199@yahoo.com>
2023-01-25 15:01:20 +0800

    only make promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,14 @@
-//only make promise
+// Only make Promise
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        //eslint-disable
-        if (true) {
-            resolve();
-        } else {
-            reject();
-        }
-        //eslint-enable
-    });
+  return new Promise((resolve, reject) => {
+    /* eslint-disable */
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+      /* eslint-enable */
+  });
 }
 
 export default getResponseFromAPI;
