dcaf748c4439d3e717f26d8ba9317fc982ae729a
-Brownlolaoo <-aramyeedey199@yahoo.com>
2023-01-25 15:29:51 +0800

    adding more commit

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+// promise is handle or not
+function handleResponseFromAPI(promise) {
+ return promise
+  .then(() => ({
+   status: 200,
+   body: 'success',
+ }))
+ .catch(() => Error())
+ .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
